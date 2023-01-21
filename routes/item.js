15f6a823e2762c94334eab2f90ca6381006ab2dd
hzk2021 <hezhenkai1999@gmail.com>
2023-01-21 12:10:09 +0800

    Add searchbox

--- routes/item.js
+++ routes/item.js
@@ -6,6 +6,7 @@ const multer = require('multer');
 const upload = multer();
 const s3Bucket = require('../configs/s3bucket');
 const { v4: uuidv4 } = require('uuid');
+const {Op, Model, where} = require('sequelize');
 
 app.get("/create", (req,res) => {
     res.render("create-item", {title: "Create Item"})
@@ -73,8 +74,38 @@ app.get("/list", async (req,res) => {
 
 });
 
-app.get("/lost", (req,res) => {
-    res.status(200).send("lost");
-});
+app.get("/search", async (req,res, next) => {
 
+    var search = req.query.search;
+
+    const condition = {
+        [Op.or]: {
+            "Name": {[Op.substring]: search},
+            "Description": {[Op.substring]: search},
+            "DateReported": {[Op.substring]: search}
+        }
+    }
+    let allItems = await Item.findAll({
+        where: condition
+    });
+
+    console.log(allItems);
+
+    if (allItems.length > 0){
+        for (let i = 0; i < allItems.length; i++) {
+            s3Bucket.getImageObject(`${allItems[i].ItemID}${allItems[i].Name}.png`, (imageData) => {
+                allItems[i].imageBase64 = Buffer.from(imageData.Body).toString('base64');  
+
+                if (i + 1 == allItems.length) {
+                    if (req.session.user) return res.render("item-list-admin", { title: "Reported Items", items: allItems});
+                    else return res.render("item-list-public", { title: "Reported Items", items: allItems});
+                }   
+            });
+        }
+    }else {
+        if (req.session.user) return res.render("item-list-admin", { title: "Reported Items", items: allItems});
+        else return res.render("item-list-public", { title: "Reported Items", items: allItems});
+    }
+
+});
 module.exports = app;
